8d61e26b29322d7f71be5d9d2f3d60fc5f7c2bff
Habiba Akter Bilashee <wbilashee@gmail.com>
2022-12-13 10:11:13 +0600

    server done

--- server/app.js
+++ server/app.js
@@ -3,6 +3,10 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const xss = require("xss-clean");
+const helmet = require("helmet");
+const rateLimiter = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
 const connectDB = require("./db/connect");
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
@@ -11,7 +15,15 @@ const notFound = require("./middleware/not-found");
 const errorHandler = require("./middleware/error-handler");
 
 // middleware
+app.set("trust proxy", 1);
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+}));
+app.use(xss());
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(helmet());
+app.use(mongoSanitize());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
